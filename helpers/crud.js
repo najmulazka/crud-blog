d0eca0b5065f70d16da854c1283e13fe87954487
Asep Hudori <asep.hudori532@gmail.com>
2023-09-25 22:10:46 +0700

    Menambahkan function destroy

--- helpers/crud.js
+++ helpers/crud.js
@@ -15,6 +15,18 @@ function show(id) {
   return post;
 }
 function update(id, title, body) {}
-function destroy(id) { }
+function destroy(id) {
+  const postIndex = posts.data.findIndex((post) => post.id === id);
+
+  if (postIndex !== -1) {
+    const deletedPost = posts.data.splice(postIndex, 1)[0];
+
+    fs.writeFileSync("./database/posts.json", JSON.stringify(posts, null, 4));
+
+    return deletedPost;
+  } else {
+    return null;
+  }
+}
 
 module.exports = { create, index, show, update, destroy };
--- index.js
+++ index.js
@@ -1,17 +1,25 @@
-const { create, index, show, update, destroy } = require('./helpers/crud');
+const { create, index, show, update, destroy } = require("./helpers/crud");
 
 // test create post
-create('test title', 'test data');
+create("test title", "test data");
 
 // test show all post
 // test show detail post by id
-const find = 1
-const found = show(find)
+const find = 1;
+const found = show(find);
 
 if (found) {
-    console.log(`Menampilkan id: ${find}`)
+  console.log(`Menampilkan id: ${find}`);
 } else {
-    console.log(`ID ${find} tidak ditemukan`)
+  console.log(`ID ${find} tidak ditemukan`);
 }
 // test update post by id
 // test delete post by id
+const hapus = 12;
+const hapusPos = destroy(hapus);
+
+if (hapusPos) {
+  console.log(`Menghapus post dengan ID ${hapus}:`, hapusPos);
+} else {
+  console.log(`Post dengan ID ${hapus} tidak ditemukan.`);
+}
