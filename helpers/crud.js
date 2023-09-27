8e83041624c9a8746a4585bca3dc3c1736231114
Asep Hudori <asep.hudori532@gmail.com>
2023-09-27 20:32:43 +0700

    Update function show dan destroy

--- helpers/crud.js
+++ helpers/crud.js
@@ -1,20 +1,32 @@
-const fs = require('fs');
-const posts = require('../database/posts');
-const PostModel = require('../models/post');
+const fs = require("fs");
+const posts = require("../database/posts");
+const PostModel = require("../models/post");
 
 function create(title, body) {
-    let newPost = new PostModel(posts.id++, title, body);
-    posts.data.push(newPost);
+  let newPost = new PostModel(posts.id++, title, body);
+  posts.data.push(newPost);
 
-    fs.writeFileSync('./database/posts.json', JSON.stringify(posts, null, 4));
+  fs.writeFileSync("./database/posts.json", JSON.stringify(posts, null, 4));
 }
 
-function index() {
-    let showAll = fs.readFileSync('./database/posts.json', 'utf-8');
-    console.log(showAll);
+function index() {}
+function show(id) {
+  const post = posts.data.find((post) => post.id === id);
+  return post;
 }
-function show(id) {}
 function update(id, title, body) {}
-function destroy(id) {}
+function destroy(id) {
+  const postIndex = posts.data.findIndex((post) => post.id === id);
+
+  if (postIndex !== -1) {
+    const deletedPost = posts.data.splice(postIndex, 1)[0];
+
+    fs.writeFileSync("./database/posts.json", JSON.stringify(posts, null, 4));
+
+    return deletedPost;
+  } else {
+    return null;
+  }
+}
 
 module.exports = { create, index, show, update, destroy };
